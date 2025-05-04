f32b2f4a496b8454436f6afb1c99a3a8beb464d7
Emilio Jaimes <emilio.jaimes.mtz2@gmail.com>
2025-05-03 22:10:35 -0600

    carga de repositorio

---
+++ Chess.Core/Pieces/Rook.cs
@@ -0,0 +1,33 @@
+﻿namespace Chess.Core.Pieces
+{
+    public class Rook : Piece
+    {
+        public Rook()
+        {
+            _symbol = 'r';
+        }
+
+        public Rook(char color, int row, int col) : base(color, row, col)
+        {
+            _symbol = 'r';
+        }
+
+        private readonly static int[][] MoveTemplates = new int[][]
+        {
+            new [] { 1, 0 }, // right
+            new [] { 0, -1 }, // left
+            new [] { 0, 1 }, // up
+            new [] { -1, 0 }, // down
+        };
+
+        public override IList<Tile> GetValidMoves(Board board)
+        {
+            return Movement.GetMoves(board, this, board.Size, MoveTemplates);
+        }
+
+		public override Piece Clone()
+		{
+			return new Rook(this.Color, this.CurrentLocation.Row, this.CurrentLocation.Column);
+		}
+	}
+}
